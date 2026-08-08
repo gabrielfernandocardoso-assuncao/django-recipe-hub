0b9349eb975f9d9648cc881963f28e1830e4d552
fernando <gabrielfcassuncao@gmail.com>
2026-08-08 20:56:37 -0300

    adicionei o formulario e a pagina que vai renderizar o formulario de new_recipe

--- recipe_hub/recipes/views.py
+++ recipe_hub/recipes/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from .models import Recipe
+from django.contrib.auth.decorators import login_required
+from .forms import RecipeForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -10,3 +14,24 @@ def index(request):
     }
 
     return render(request, 'recipes/index.html', context)
+
+@login_required()
+def new_recipe(request):
+    form = RecipeForm(request.POST, request.FILES)
+
+    if request.method != 'POST':
+        form = RecipeForm()
+    else:
+        form = RecipeForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            new_recipe = form.save(commit=False)
+
+            new_recipe.owner = request.user
+            new_recipe.save()
+
+            return HttpResponseRedirect(reverse('index'))
+
+    context = {'form':form}
+
+    return render(request, 'recipes/new_recipe.html', context)
