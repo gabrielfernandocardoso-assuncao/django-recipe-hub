87a43e5c212735e46f1cf842259a3d3c7316a46b
fernando <gabrielfcassuncao@gmail.com>
2026-08-08 11:31:26 -0300

    fiz as preparações e o banco de dados

---
+++ recipe_hub/recipes/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class recipe(models.Model):
+    # title
+    title = models.CharField(max_length=200)
+    # ingredients
+    ingredients = models.TextField()
+    # instructions
+    instructions = models.TextField()
+    # prep_time
+    prep_time = models.IntegerField()
+    # owner
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    # img
+    image = models.ImageField(upload_to='recipe_images/', blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = 'recipes'
+
+    def __str__(self):
+        return self.title 
